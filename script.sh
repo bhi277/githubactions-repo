e077963c8e6721e33dd0b4d00a4ed6863f9136d8
linuxadviserb <aparthan277@gmail.com>
2023-12-20 23:25:25 +0530

    8th commit

--- script.sh
+++ script.sh
@@ -2,3 +2,10 @@
 #!/bin/bash
 
 echo "Its abhiraj parthan"
+
+
+sudo su
+yum -y install apache2
+echo "<p> Its me Abhiraj  </p>" >> /var/www/html/index.html
+sudo systemctl enable apache2
+sudo systemctl start apache2
